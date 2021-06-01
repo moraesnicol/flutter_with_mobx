77cb22464d1feb055149901c42bc1c915c20818b
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-31 22:11:19 -0300

    mobx init

---
+++ lib/controler.dart
@@ -0,0 +1,19 @@
+import 'package:mobx/mobx.dart';
+
+class Controller {
+  var counter = Observable(0);
+  Action increment;
+
+  Controller() {
+    increment = Action(_increment);
+
+    autorun((_) {
+      print(counter.value);
+     
+    });
+  }
+
+  _increment() {
+    counter.value = counter.value + 5;
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_with_mobx/controler.dart';
+
+class HomePage extends StatelessWidget {
+  final controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MobX"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(builder: (_) {
+              return Text(
+                '${controller.counter.value}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.increment();
+        },
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
